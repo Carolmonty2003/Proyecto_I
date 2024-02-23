7b8a910e359b27e6570759ce3595ffc704b8525f
Eduard Bastida <eduard.bastida@enti.cat>
2024-02-23 09:14:24 +0100

    2

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-
+    public float movementSpeed = 5;
+    public float jumpForce = 300.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    GetComponent<Rigidbody2D>().velocity = new Vector2(0,5);
+        //}
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(2, 0));
+            
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-2, 0));
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-2, 0);
+            //transform.position += new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+            }
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1);
+        }
+        
     }
 }
